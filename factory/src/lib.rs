d6a28b1f7223b18c92b92aa8367f2fdb9ca39800
mootz12 <mootz12@protonmail.com>
2024-04-18 21:06:05 -0400

    chore: update factory with init changes

--- factory/src/lib.rs
+++ factory/src/lib.rs
@@ -1,31 +1,20 @@
 #![no_std]
-#![allow(unused)]
+
 use call_logic::factory::{execute_is_c_pool, execute_new_c_pool};
 use soroban_sdk::{
-    assert_with_error, contract, contractimpl, contracttype, symbol_short, unwrap::UnwrapOptimized,
-    vec, Address, Bytes, BytesN, Env, IntoVal, Symbol, Val, Vec,
+    assert_with_error, contract, contractimpl, contracttype, Address, BytesN, Env, Vec,
 };
 
 // Errors Listed
 pub mod call_logic;
 pub mod error;
-use crate::{
-    call_logic::factory::{execute_collect, execute_init, execute_set_c_admin},
-    error::Error,
-};
-
-pub(crate) const DAY_IN_LEDGERS: u32 = 17280;
-pub(crate) const SHARED_BUMP_AMOUNT: u32 = 69120; // 4 days
-pub(crate) const SHARED_LIFETIME_THRESHOLD: u32 = SHARED_BUMP_AMOUNT - DAY_IN_LEDGERS;
-pub(crate) const LARGE_BUMP_AMOUNT: u32 = 518400; // 30 days
-pub(crate) const LARGE_LIFETIME_THRESHOLD: u32 = LARGE_BUMP_AMOUNT - DAY_IN_LEDGERS;
+use crate::{call_logic::factory::execute_init, error::Error};
 
 // Keys which will give access to the corresponding data
 #[derive(Clone)]
 #[contracttype]
 pub enum DataKeyFactory {
     IsCpool(Address),
-    Admin,
     WasmHash,
 }
 
@@ -37,74 +26,38 @@ pub struct NewPoolEvent {
     pub pool: Address,
 }
 
-// Event to signal the admin for the factory contract has been changed.
-#[contracttype]
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct SetAdminEvent {
-    pub caller: Address,
-    pub admin: Address,
-}
 #[contract]
 pub struct Factory;
 
 #[contractimpl]
 impl Factory {
     // Initialize the Admin for the Factory Contract
-    pub fn init(e: Env, user: Address, pool_wasm_hash: BytesN<32>) {
+    pub fn init(e: Env, pool_wasm_hash: BytesN<32>) {
         assert_with_error!(
             &e,
-            !e.storage().instance().has(&DataKeyFactory::Admin),
+            !e.storage().instance().has(&DataKeyFactory::WasmHash),
             Error::AlreadyInitialized
         );
-        user.require_auth();
-        execute_init(e, user, pool_wasm_hash);
+        execute_init(e, pool_wasm_hash);
     }
 
     // Create a new Comet Pool
-    pub fn new_c_pool(e: Env, salt: BytesN<32>, user: Address) -> Address {
-        user.require_auth();
-        execute_new_c_pool(e, salt, user)
-    }
-
-    // Set a new admin for the factory contract
-    pub fn set_c_admin(e: Env, caller: Address, user: Address) {
-        assert_with_error!(
-            &e,
-            caller
-                == e.storage()
-                    .instance()
-                    .get::<DataKeyFactory, Address>(&DataKeyFactory::Admin)
-                    .unwrap_optimized(),
-            Error::ErrNotController
-        );
-        caller.require_auth();
-        execute_set_c_admin(e, caller, user);
-    }
-
-    pub fn collect(e: Env, caller: Address, addr: Address) {
-        assert_with_error!(
-            &e,
-            caller
-                == e.storage()
-                    .instance()
-                    .get::<DataKeyFactory, Address>(&DataKeyFactory::Admin)
-                    .unwrap_optimized(),
-            Error::ErrNotController
-        );
-        caller.require_auth();
-        execute_collect(e, caller, addr)
+    pub fn new_c_pool(
+        e: Env,
+        salt: BytesN<32>,
+        controller: Address,
+        tokens: Vec<Address>,
+        weights: Vec<i128>,
+        balances: Vec<i128>,
+        swap_fee: i128,
+    ) -> Address {
+        controller.require_auth();
+        execute_new_c_pool(e, salt, controller, tokens, weights, balances, swap_fee)
     }
 
     // Returns true if the passed Address is a valid Pool
     pub fn is_c_pool(e: Env, addr: Address) -> bool {
         execute_is_c_pool(e, addr)
-    }
-    // Get the Current Admin of the Factory Contract
-    pub fn get_c_admin(e: Env) -> Address {
-        e.storage()
-            .instance()
-            .get::<DataKeyFactory, Address>(&DataKeyFactory::Admin)
-            .unwrap_optimized()
     }
 }
 
